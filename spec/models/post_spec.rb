53ec946f40551366062f4cf792867bd5d3999391
Anders Törnqvist <anders.tornqvist@gmail.com>
2011-12-16 16:02:48 +0100

    Add validations to title.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe Post do
-  let(:post) { Post.make.new }
+  let(:post) { Post.make.save }
 
   specify { post.should be_valid }
+
+  describe "#title" do
+    it "must be present" do
+      Post.make(title: nil).should have(1).error_on(:title)
+      Post.make(title: "  ").should have(1).error_on(:title)
+    end
+
+    it "must be unique" do
+      Post.make(title: post.title).should have(1).error_on(:title)
+    end
+  end
 end
