f279e41a8897ccde50dccbac034278d7ffb6c314
Anders Törnqvist <anders.tornqvist@gmail.com>
2011-12-16 14:03:14 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JayzRspecBlog::Application.config.secret_token = 'a8152e0c0a170c6986ffdff30e1352cb05491831e5d205d833e47790449ba6f00207f0c6590ed0bb8b36c4e24b957a634c2bdedece1c59f2b01f5c4127e069f6'
